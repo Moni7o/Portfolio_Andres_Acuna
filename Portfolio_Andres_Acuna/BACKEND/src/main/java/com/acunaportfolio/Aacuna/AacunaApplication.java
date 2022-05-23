15ea2aaa983dd2528c962287a8a10b36bffd2bb4
Moni7o <94930048+Moni7o@users.noreply.github.com>
2022-05-23 02:04:58 -0300

    backend

---
+++ Portfolio_Andres_Acuna/BACKEND/src/main/java/com/acunaportfolio/Aacuna/AacunaApplication.java
@@ -0,0 +1,13 @@
+package com.acunaportfolio.Aacuna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AacunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AacunaApplication.class, args);
+	}
+
+}
---
+++ Portfolio_Andres_Acuna/BACKEND/src/test/java/com/acunaportfolio/Aacuna/AacunaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acunaportfolio.Aacuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AacunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
