8cc3f350b0acc3c7d49f5145247da2ad4f416218
Moni7o <94930048+Moni7o@users.noreply.github.com>
2022-05-24 02:27:36 -0300

    8080 front

---
+++ Portfolio_Andres_Acuna/FRONTEND/src/app/model/persona.model.ts
@@ -0,0 +1,13 @@
+export class persona{
+    id?: number;
+    nombre: string;
+    apellidos: string;
+    img: string;
+
+    constructor(nombre: string,apellido: string,img: string){
+        this.nombre = nombre;
+        this.apellidos = apellido;
+        this.img = img;
+    }
+}
+
