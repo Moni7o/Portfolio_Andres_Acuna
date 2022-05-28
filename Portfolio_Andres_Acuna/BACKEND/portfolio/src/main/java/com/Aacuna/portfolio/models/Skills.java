63f62a8d6aecc064f85bb54408c09ddc5ad2f68a
Moni7o <94930048+Moni7o@users.noreply.github.com>
2022-05-28 02:49:41 -0300

    update 28/5

---
+++ Portfolio_Andres_Acuna/BACKEND/portfolio/src/main/java/com/Aacuna/portfolio/models/Skills.java
@@ -0,0 +1,63 @@
+
+package com.Aacuna.portfolio.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Skills {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idSkill;
+    private String nombreSkill;
+    private String imgSkill;
+    private int porcentaje;
+
+    public Skills() {
+    }
+
+    public Skills(Long idSkill, String nombreSkill, String imgSkill, int porcentaje) {
+        this.idSkill = idSkill;
+        this.nombreSkill = nombreSkill;
+        this.imgSkill = imgSkill;
+        this.porcentaje = porcentaje;
+    }
+
+    public Long getIdSkill() {
+        return idSkill;
+    }
+
+    public void setIdSkill(Long idSkill) {
+        this.idSkill = idSkill;
+    }
+
+    public String getNombreSkill() {
+        return nombreSkill;
+    }
+
+    public void setNombreSkill(String nombreSkill) {
+        this.nombreSkill = nombreSkill;
+    }
+
+    public String getImgSkill() {
+        return imgSkill;
+    }
+
+    public void setImgSkill(String imgSkill) {
+        this.imgSkill = imgSkill;
+    }
+
+    public int getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(int porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+    
+    
+    
+    
+}
---
+++ Portfolio_Andres_Acuna/BACKEND/portfolio/src/main/java/com/Aacuna/portfolio/models/Usuario.java
@@ -0,0 +1,94 @@
+
+package com.Aacuna.portfolio.models;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Usuario implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String titulo;
+    private String descripcion;
+    private String imgPerfil;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idEdu")
+    public List<Educacion> educacionList;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idExp")
+    public List<Experiencia> experienciaList;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "idSkill")
+    public List<Skills> skillList;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String nombre, String apellido, String titulo, String descripcion, String imgPerfil) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.imgPerfil = imgPerfil;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImgPerfil() {
+        return imgPerfil;
+    }
+
+    public void setImgPerfil(String imgPerfil) {
+        this.imgPerfil = imgPerfil;
+    }
+    
+    
+}
